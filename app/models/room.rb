0412dd80e2009f8b7d6017dd724fcf2ad25dda77
mstk7342 <m7s3t4k228@gmail.com>
2020-08-16 16:14:49 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,8 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+
+  validates :name, presence: true
+  # validates :カラム名, 検証ルール（シンボル指定「:」）
+  # ルーム名が存在(presence)している場合のみ作成可能(ture)という意味
 end
