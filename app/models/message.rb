d28599d307fd7b58ed13e54232e596346de5b537
mstk7342 <m7s3t4k228@gmail.com>
2020-08-20 01:04:29 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,6 +6,11 @@ class Message < ApplicationRecord
   # １人のユーザーは、複数のメッセージを送信できる。
   has_one_attached :image
   # Messageテーブルの各レコードと画像ファイルを管理するimageカラムの紐付けができる。Messageテーブルにカラムを追加する必要はない。
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
   # 「content」カラムに、presence: tureを設けることで、空の場合DBに保存しないバリデーション設定をしている。
+
+  def was_attached?
+    self.image.attached?
+  end
+  # バリデーションに「unless: :was_attached?」を追記して、記述すると画像のみの送信が可能。
 end
